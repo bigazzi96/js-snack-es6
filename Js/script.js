377ea1b8aa9972f0b043e5c528ef3e2b96caf732
bigazzi96 <fbigazzi96@gmail.com>
2024-05-23 18:10:06 +0200

    aggiunto array  e funzione per nomi maiuscolo

--- Js/script.js
+++ Js/script.js
@@ -1,4 +1,7 @@
 
+
+//SNACK 1
+
 //* array con i nomi dei Vip
 const star = [
     'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 
@@ -19,3 +22,51 @@ const TavoloVip = star.map((guest, index) => {
 });
 
 console.log(TavoloVip);
+
+
+//SNACK 2
+
+//*array
+const aula = [
+    {
+        Id: 213,
+        Name: 'Giuseppina della Rovere',
+        Grades: 78
+    },
+    {
+        Id: 110,
+        Name: 'Paola Cortellessa',
+        Grades: 96
+    },
+    {
+        Id: 250,
+        Name: 'Andrea Mantegna',
+        Grades: 48
+    },
+    {
+        Id: 145,
+        Name: 'Gaia Borromini',
+        Grades: 74
+    },
+    {
+        Id: 196,
+        Name: 'Luigi Grimaldello',
+        Grades: 68
+    },
+    {
+        Id: 102,
+        Name: 'Piero della Francesca',
+        Grades: 50
+    },
+    {
+        Id: 120,
+        Name: 'Francesca da Polenta',
+        Grades: 84
+    }
+];
+
+//*funzione per rendere i nomi in maiuscolo
+const nomiMaiuscolo = aula.map(studente => studente.Name.toUpperCase());
+console.log(nomiMaiuscolo);
+
+
